fc7523fffb07c466fae645b0067e066167d80426
Christopher Rolke <christopher.rolke@edu.fh-joanneum.at>
2021-12-27 17:30:57 +0100

    Backend: Serializer.py created, views.py initialized

---
+++ backend/firebit_api/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+
+from firebit_api.models import Bit, Bookmark, Category, Comment, Image, Like, Friendship, FriendshipStatus
+
+
+class BitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bit
+        fields = '__all__'
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookmark
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = '__all__'
+
+
+class FriendshipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Friendship
+        fields = '__all__'
+
+
+class FriendshipStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FriendshipStatus
+        fields = '__all__'
--- backend/firebit_api/views.py
+++ backend/firebit_api/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+import json
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.response import Response
 
-# Create your views here.
+from . import models
+from .serializers import *
+
+
+class BitViewSet(viewsets.ViewSet):
+
+    def list(self, request, format=None):
+        queryset = models.Bit.objects.all()
+
+        serializer = BitSerializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+
+    def create(self, request, format=None):
+
+        serializer = BitSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=201
+            )
+        else:
+            return Response(serializer.errors, status=400)
+
+    def retrieve(self, request, pk=None, format=None):
+        try:
+            bit = models.Bit.objects.get(
+                pk=pk
+            )
+            serializer = BitSerializer(bit)
+            return Response(serializer.data, status=200)
+
+        except models.Bit.DoesNotExist:
+            return Response(status=404)
+
+    def update(self, request, pk=None, format=None):
+        try:
+            bit = models.Bit.objects.get(
+                pk=pk
+            )
+            serializer = BitSerializer(bit, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data,
+                    status=201
+                )
+            else:
+                return Response(serializer.errors, status=400)
+        except models.Bit.DoesNotExist:
+            return Response(status=404)
+
+    def partial_update(self, request, pk=None, format=None):
+        # We do not allow partial updates here
+        # So we return a 405 instead.
+        return Response(status=405)
+
+    def destroy(self, request, pk=None, format=None):
+        try:
+            bit = models.Bit.objects.filter(
+                pk=pk
+            ).delete()
+        except models.Bit.DoesNotExist:
+            return Response(status=404)
+
+        return Response(status=204)
