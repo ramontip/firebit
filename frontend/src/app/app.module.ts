e18ca66283adcbee07def29a246562293bce70db
Xeeija <19950592+xeeija@users.noreply.github.com>
2022-01-25 23:27:14 +0100

    Fix terms and about page design

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -67,7 +67,8 @@ import {SearchPageComponent} from './pages/search-page/search-page.component';
 import {TermsPageComponent} from './pages/terms-page/terms-page.component';
 import {AdminPageComponent} from './pages/admin-page/admin-page.component';
 import {MatTableModule} from "@angular/material/table"
-import {MatTooltipModule} from "@angular/material/tooltip"
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {FooterComponent} from './components/footer/footer.component'
 
 function tokenGetter() {
   return localStorage.getItem('access_token');
@@ -117,6 +118,7 @@ function tokenGetter() {
     SearchPageComponent,
     TermsPageComponent,
     AdminPageComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
